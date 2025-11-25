aa19b94cb7a9ddd0d4d547cb96e50fd2d27d6b0b
JakkalaRamu2005 <jakkalaramu233@gmail.com>
2025-11-25 11:38:25 +0530

    added stylesglobals

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/globals.css';
+import './styles/components.css';
 
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  
-)
+  </React.StrictMode>
+);
